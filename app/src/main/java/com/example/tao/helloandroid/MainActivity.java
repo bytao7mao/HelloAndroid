d7545abaac79a48c6ba3f88b63dd808fba2f8cc1
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2017-12-04 20:45:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tao/helloandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tao.helloandroid;
+
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.net.URL;
+
+import static com.example.tao.helloandroid.R.id.imageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
