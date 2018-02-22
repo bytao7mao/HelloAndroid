c9f387c3f748da62d8fde8e27a58e877b2c666d4
Marius Nicolae <marius.a.nicolae@outlook.com>
2018-02-22 19:10:34 +0200

    version nanoDegree

--- app/src/main/java/com/example/tao/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/tao/helloandroid/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.tao.helloandroid;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.net.URL;
 
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView noteView = (TextView) findViewById(R.id.phoneId);
+        noteView.setText("+40768922335");
+        Linkify.addLinks(noteView, Linkify.ALL);
     }
 
 
